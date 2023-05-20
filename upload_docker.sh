d908fc4c4e99ea14b01b1da9cff4c0362b02ec95
Gaurav Kumar <geekay@192.168.1.6>
2023-05-21 02:08:47 +0530

    Added New files

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=gauravkr04/udacityblue
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag 2180dad77e11 $dockerpath:v1.0.0
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:v1.0.0
